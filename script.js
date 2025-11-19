469d39bbfafc56519fc9c38f0278ebe4f25dedaf
bright123-max <brightathuman65@gmail.com>
2025-11-19 13:36:34 +0300

    transforming sign up and login button to navigation bar at the top

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-// function showGreeting() {
-//   const demo = document.getElementById('greetings');
-//   if (!demo) return; // nothing to do if the target element is missing
+"use strict";
+function showGreeting() {
+  const hour = new Date().getHours();
+  if (hour >= 5 && hour < 12) {
+    return 'Good Morning,';
+  } 
+  else if (hour >= 12 && hour < 18) {
+     return 'Good Afternoon,';
+  }
+   else if (hour >= 18 && hour < 22) {
+    return 'Good Evening,';
+  }
+   else {
+   return 'Good Night,';
+  }
+}
+ var welcome = document.getElementById('greeting');
+welcome.innerHTML=showGreeting() + " welcome to our hotel,Enjoy your special occassion here";
 
-//   const hour = new Date().getHours();
-//   let greeting = 'Welcome';
+var modal = document.getElementById('id01');
 
-//   if (hour >= 5 && hour < 12) {
-//     greeting = 'Good Morning';
-//   } else if (hour >= 12 && hour < 18) {
-//     greeting = 'Good Afternoon';
-//   } else if (hour >= 18 && hour < 22) {
-//     greeting = 'Good Evening';
-//   } else {
-//     greeting = 'Good Night';
-//   }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
-// Get the modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
